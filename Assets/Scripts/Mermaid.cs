6310009aafb8ca5d76d00163f3bce08902da3549
UISU <hanaaple@ajou.ac.kr>
2021-07-24 20:08:32 +0900

    Update Mermaid.cs

--- Assets/Scripts/Mermaid.cs
+++ Assets/Scripts/Mermaid.cs
@@ -16,9 +16,9 @@ public class Mermaid : MonoBehaviour
     private List<ExpressionSprites> mermaidSpriteList;
     private Image image;
 
-    public Data.ICE[] ice = new Data.ICE[2];
-    public Data.SYRUP[] syrup = new Data.SYRUP[2];
-    public Data.TOPPING[] topping = new Data.TOPPING[2];
+    public Data.ICE[] ice;
+    public Data.SYRUP[] syrup;
+    public Data.TOPPING[] topping;
 
     public int bingsuCount; //?? ??
 
@@ -30,11 +30,14 @@ public class Mermaid : MonoBehaviour
     {
         if (!gameObject.TryGetComponent(out image))
             Debug.LogError("Image ????? ?? ? ????");
+        ice = new Data.ICE[2];
+        syrup = new Data.SYRUP[2];
+        topping = new Data.TOPPING[2];
     }
 
     public bool CompareBingsu(Data.ICE selectedIce, Data.SYRUP selectedSyrup, Data.TOPPING selectedTopping)
     {
-        if(selectedIce == ice[0] && selectedSyrup == syrup[0] && selectedTopping == topping[0])
+        if (selectedIce == ice[0] && selectedSyrup == syrup[0] && selectedTopping == topping[0])
         {
             //UI update remove wanted bingsu
             return true;
@@ -45,7 +48,10 @@ public class Mermaid : MonoBehaviour
             return true;
         }
         else
+        {
+            Debug.Log("½ÇÆÐ");
             return false;
+        }
     }
 
     // level? ?? ??? ??
@@ -72,10 +78,15 @@ public class Mermaid : MonoBehaviour
 
 
         //??? ??? ?? ice, ??, topping ? ??
+        for(int i = 0; i < bingsuCount; i++)
+        {
+            //StageManager.instance.IngredientUnlockData.GetRandomIce
+        }
+
     }
 
     public void SetExpression(EXPRESSION expression)
     {
-        image.sprite = mermaidSpriteList[mermaidIndex].expression[((int)expression)];
+        image.sprite = mermaidSpriteList[mermaidIndex].expression[(int)expression];
     }
 }
