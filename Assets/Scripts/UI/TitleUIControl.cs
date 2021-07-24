fc4c3ec74e16e15711a65dbb7402b73621e09efe
tyrosine1153 <tyrosine1153@gmail.com>
2021-07-24 16:19:10 +0900

    빼먹은 스크립트 추가
    죄송합니다!

---
+++ Assets/Scripts/UI/TitleUIControl.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleUIControl : MonoBehaviour
+{
+    public void OnClickStartButton()
+    {
+        SceneLoadManager.Instance.LoadNextScene();
+    }
+}
